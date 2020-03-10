8f493a5c831117cbce818ec85d402b4addd0e3b9
HikaruEgashira <shighten@gmail.com>
2020-03-11 01:50:10 +0900

    level 2

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,37 @@
-import { mapFilterTake } from '../src'
-
 jest.setTimeout(10000)
 
-describe('blah', () => {
+import xs from 'xstream'
+
+describe('stream', () => {
   it('works', async () => {
-    await mapFilterTake()
-    expect(1).toEqual(1)
+    const stream = xs
+      .periodic(500)
+      .filter(i => i % 2 === 0)
+      .map(i => i * i)
+      .endWhen(xs.periodic(5000).take(1))
+      .debug(console.log)
+
+    await new Promise(done => {
+      stream.addListener({
+        next: void 0,
+        error: console.error,
+        complete: done,
+      })
+    })
+  })
+
+  it('jest special', done => {
+    const stream = xs
+      .periodic(500)
+      .filter(i => i % 2 === 0)
+      .map(i => i * i)
+      .endWhen(xs.periodic(5000).take(1))
+      .debug(console.log)
+
+    stream.addListener({
+      next: void 0,
+      error: done,
+      complete: done,
+    })
   })
 })
